e028f9f04bb2aeb04da23ab6c45ff4228d97db86
MadsAW <40065771+MadsAW@users.noreply.github.com>
2019-11-11 15:21:10 +0100

    Update controller.py

--- scripts/controller.py
+++ scripts/controller.py
@@ -12,18 +12,28 @@ hidden_dim = 50
 
 
 
-class Net(nn.Module):
+class Controller(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-
+        # Create tokens which maps the amount of options for each layer
         # Recurrent layer
-        # YOUR CODE HERE!
         self.lstm = nn.LSTM(hidden_dim,hidden_dim)
+
+        #Linear layer for each of the blocks
+        self.num_tokens = [len(args.shared_rnn_activations)]
+        for idx in range(self.args.num_blocks):
+            self.num_tokens += [len(args.sizes_dict), len(args.activations_dict)]
+        for i in self.num_blocks:
+            decoder = torch.nn.Linear(args.controller_hid, self.size)
+            self.decoders.append(decoder)
+        for idx, size in enumerate(self.num_tokens):
+            decoder =
+            self.decoders.append(decoder)
 
     def forward(self, x):
         # unsqueeze to have correct dim for lstm
         x = x.unsqueeze(0)
-        x, (h, c) = self.lstm(x)
+        x, h = self.lstm(x)
         return x
 
 
