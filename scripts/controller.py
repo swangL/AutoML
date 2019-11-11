1f1066ab5c802ca42edebb41aaa480bbf31d48a4
MadsAW <40065771+MadsAW@users.noreply.github.com>
2019-11-11 16:41:06 +0100

    Update controller.py

--- scripts/controller.py
+++ scripts/controller.py
@@ -3,39 +3,78 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-
-hidden_dim = 50
-
 #Find how to handle input
 
 #How to controlle the output
 
-
-
+#args implementation
+sizes_dict={
+    0:"term",
+    1:"Dense(1)",
+    2:"Dense(2)",
+}
+activations_dict={
+    0:"relu",
+    1:"tanh",
+    2:"sigmoid",
+}
+num_blocks=5
+hidden_dim = 50
 class Controller(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(Controller, self).__init__()
         # Create tokens which maps the amount of options for each layer
         # Recurrent layer
         self.lstm = nn.LSTM(hidden_dim,hidden_dim)
 
-        #Linear layer for each of the blocks
-        self.num_tokens = [len(args.shared_rnn_activations)]
-        for idx in range(self.args.num_blocks):
-            self.num_tokens += [len(args.sizes_dict), len(args.activations_dict)]
-        for i in self.num_blocks:
-            decoder = torch.nn.Linear(args.controller_hid, self.size)
-            self.decoders.append(decoder)
-        for idx, size in enumerate(self.num_tokens):
-            decoder =
+        #Linear layer for each of the block - decoder
+        self.decoders=[]
+        self.num_tokens = [len(activations_dict)]
+        for i in range(num_blocks):
+            # TODO (Mads): implement variable amount of choices / help guide
+            self.num_tokens += [len(sizes_dict), len(activations_dict)]
+        for size in self.num_tokens:
+            decoder = torch.nn.Linear(hidden_dim, size)
             self.decoders.append(decoder)
 
-    def forward(self, x):
+    def forward(self, inputs, hidden, block_id):
         # unsqueeze to have correct dim for lstm
-        x = x.unsqueeze(0)
-        x, h = self.lstm(x)
-        return x
+        h, c = self.lstm(inputs)#, hidden)
+        logits = self.decoders[block_id](h)
+        # TODO(Mads): Softmax temperature and added exploration:
+        # if self.args.mode == 'train':
+        #     logits = (tanh_c*F.tanh(logits))
+        # logits, hidden
+        return logits.squeeze(), (h, c)
 
+    def sample(self):
+        # default input
+        inputs = torch.Tensor(1, hidden_dim)
+        hidden = torch.Tensor(1, 1, hidden_dim)
+        inputs = inputs.unsqueeze(0)
+        activations = []
+        nodes = []
+        # 1 block includes hidden and activation as such num_block*2 + 1 - 1 since we want to append Dense
+        for block_id in range(num_blocks*2):
+            #handle terminate argument
+            #parse last hidden using overwrite
+            logits, hidden = self.forward(inputs, hidden, block_id)
+            # use logits to make choice
+            probs = F.softmax(logits, dim=-1)
+            # we need log prob for reward
+            log_prob = F.log_softmax(logits, dim=-1)
+            # draw from probs
+            action = probs.multinomial(num_samples=1).data
+            # determine whether activation or hidden
+            if block_id%2==0:
+                activations.append(activations_dict[int(action)])
+            else:
+                value = sizes_dict[int(action)]
+                if value=="term":
+                    break
+                else:
+                    nodes.append(value)
 
-net = Net()
-print(net)
+        #child = self.create_model(activations, nodes)
+        return activations, nodes
+        #return logits, log_prob
