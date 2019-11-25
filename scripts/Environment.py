434479b860646cb7f83536cadc5d2f56ecef5e52
MadsAW <40065771+MadsAW@users.noreply.github.com>
2019-11-25 14:02:08 +0100

    trying again

--- scripts/Environment.py
+++ scripts/Environment.py
@@ -15,13 +15,13 @@ def accuracy(ys, ts):
     # making a one-hot encoded vector of correct (1) and incorrect (0) predictions
     ys = torch.argmax(ys,dim=-1)
     ts = torch.argmax(ts,dim=-1)
-    ts = ts.as_type(ys)
+    ts = ts.to(torch.FloatTensor)
     print(ys.get_device())
     print(ts.get_device())
     print(torch.eq(ys,ts).get_device())
     print(torch.eq(ys,ts).type(torch.FloatTensor).get_device())
     print(torch.mean(torch.eq(ys,ts).type(torch.FloatTensor)).get_device())
-    return torch.mean(torch.eq(ys,ts).type(torch.FloatTensor)).cpu().data.numpy()
+    return torch.mean(torch.eq(ys,ts).type(torch.FloatTensor)).data.numpy()
 
 def onehot(t, num_classes):
     out = np.zeros((t.shape[0], num_classes))
