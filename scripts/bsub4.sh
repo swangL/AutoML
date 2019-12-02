af4be9d76656c2b04a3fb2fd622b9f2834afdc98
MadsAW <40065771+MadsAW@users.noreply.github.com>
2019-12-02 11:02:25 +0100

    submission files

--- scripts/bsub4.sh
+++ scripts/bsub4.sh
@@ -12,5 +12,5 @@ module load cuda/8.0
 module load cudnn/v7.0-prod-cuda8
 
 echo "Running script..."
-python3 train.py 10000 MOONS 0.01 antict
-python3 train.py 10000 MNIST 0.01 antict
+python3 train.py 10000 MOONS 0.01 antitct
+python3 train.py 10000 MNIST 0.01 antitct
