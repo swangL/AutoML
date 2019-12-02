63c55bb55b4051f0276317035483a6a1ab347866
MadsAW <40065771+MadsAW@users.noreply.github.com>
2019-12-02 10:55:12 +0100

    changed submission files

--- scripts/bsub1.sh
+++ scripts/bsub1.sh
@@ -5,12 +5,12 @@
 #BSUB -n 1
 #BSUB -W 10:00
 #BSUB -R "rusage[mem=6GB]"
-#BSUB -o ../logs/gru%J.out
+#BSUB -o ../logs/gruco%J.out
 
 module load python3/3.6.2
 module load cuda/8.0
 module load cudnn/v7.0-prod-cuda8
 
 echo "Running script..."
-python3 train.py 10000 MOONS 0.01 egct
-python3 train.py 10000 MNIST 0.01 egct
+python3 train.py 10000 MOONS 0.01 ect
+python3 train.py 10000 MNIST 0.01 ect
